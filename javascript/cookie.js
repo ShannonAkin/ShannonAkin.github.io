4e81b24ee41aeec25e246658f26aaa05b7e62fa1
ShannonAkin <akin.shannon@hotmail.com>
2016-09-26 22:24:12 -0600

    Add files via upload

--- javascript/cookie.js
+++ javascript/cookie.js
@@ -33,7 +33,7 @@ function checkCookie() {
 function setUserName() {
     checkCookie();
     if(userName != ""){
-	document.getElementById("btnUserName").textcontent = userName;
+	document.getElementById("btnUserName").innerHTML = userName;
 	document.getElementById("btnUserName").disabled = true;
     }
 }
